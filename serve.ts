5ea3a29c646cba30e83581b9aa68d4393352a47c
Jason McCallister <jason@mccallister.dev>
2026-03-25 12:31:25 -0400

    add serve.ts helper, refresh the ux for demo
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- serve.ts
+++ serve.ts
@@ -1,6 +1,8 @@
 const apiBase = Deno.env.get("API_BASE") ?? "http://localhost:5173";
-const html = (await Deno.readTextFile(new URL("./index.html", import.meta.url)))
-  .replace("__API_BASE__", apiBase);
+
+const html = (
+  await Deno.readTextFile(new URL("./index.html", import.meta.url))
+).replace("__API_BASE__", apiBase);
 
 Deno.serve({ port: 8080 }, (_req) => {
   return new Response(html, {
