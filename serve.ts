4d6b125c14d2ce0d2f09807dfe76bbe7cf019432
Jason McCallister <jason@mccallister.dev>
2026-03-25 11:53:15 -0400

    add serve script and sorting options
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

---
+++ serve.ts
@@ -0,0 +1,9 @@
+const apiBase = Deno.env.get("API_BASE") ?? "http://localhost:5173";
+const html = (await Deno.readTextFile(new URL("./index.html", import.meta.url)))
+  .replace("__API_BASE__", apiBase);
+
+Deno.serve({ port: 8080 }, (_req) => {
+  return new Response(html, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+});
