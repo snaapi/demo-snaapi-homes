1db14085ee037177efad02187c26682805271ae3
Jason McCallister <jason@mccallister.dev>
2026-03-25 12:58:57 -0400

    pr feedback
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- serve.ts
+++ serve.ts
@@ -1,8 +1,10 @@
 const apiBase = Deno.env.get("API_BASE") ?? "http://localhost:5173";
 
+const escapedApiBase = JSON.stringify(apiBase).slice(1, -1);
+
 const html = (
   await Deno.readTextFile(new URL("./index.html", import.meta.url))
-).replace("__API_BASE__", apiBase);
+).replace("__API_BASE__", escapedApiBase);
 
 Deno.serve({ port: 8080 }, (_req) => {
   return new Response(html, {
